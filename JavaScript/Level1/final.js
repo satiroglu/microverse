a362531630869f2d48ad8257b458d55f37da5f00
Ahmet Erturk <aesatiroglu@gmail.com>
2022-04-08 01:18:03 +0000

    final assessment answers

---
+++ JavaScript/Level1/final.js
@@ -0,0 +1,34 @@
+/*
+These codes are not gonna be working on your local machine. They are just working on frecodecamp.org
+They are just answers of Level 1 Javascript final assessment.
+*/
+
+
+//////// 1. Exercise
+// Setup
+var a;
+
+// Only change code below this line
+a = 7; // The solution
+
+
+//////// 2. Exercise
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a; // The solution
+
+
+//////// 3. Exercise - All of them are solutions.
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
