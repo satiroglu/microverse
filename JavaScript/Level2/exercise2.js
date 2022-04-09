aa3aff035089d5fb7f15b6445a8563bba007e2b1
Ahmet Erturk <aesatiroglu@gmail.com>
2022-04-09 03:07:44 +0000

    added leve2 exercise 2

---
+++ JavaScript/Level2/exercise2.js
@@ -0,0 +1,15 @@
+let X = 32;
+if (X === 9) {
+  console.log("True");
+} else {
+  console.log("False");
+};
+
+let color = "Green";
+if (color === "Blue") {
+  console.log("Blue detected!");
+} else if (color === "Green") { 
+  console.log("Green detected!");
+} else { 
+  console.log("No blue or green detected!");
+}
